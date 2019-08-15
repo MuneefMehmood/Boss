3f545edd25792517e13a58088e5eda0ae167230a
Muneef <rai.muneef@yahoo.com>
2019-08-15 16:52:44 -0400

    decent boy

--- src/IfElseApplication.java
+++ src/IfElseApplication.java
@@ -4,27 +4,46 @@ public class IfElseApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int MarksObtained;
-		int PassingMarks;
-		
-		PassingMarks=40;
-		
+
+		int a;
+		int b = 40;
+
 		Scanner input = new Scanner(System.in);
-		System.out.println("Result Of Class");
-		
-		System.out.println("Input marks scored by u");
-		MarksObtained = input.nextInt();
-		
-		if (MarksObtained>=PassingMarks) {
+		System.out.println("Enter Your number");
+		a = input.nextInt();
+
+	System.out.println("--------WHILE LOOP------------------------");
+
+		while (a <= b) {
+			System.out.println("How many times has it printed---> " + a);
+			++a;
+		}
+
+	System.out.println("-----below is doWhile LOOP---------------------------");
+
+		do {
+			System.out.println("At least prints it once ---> " + a);
+
+		} while (a <= b);
+
+	System.out.println("-----below is FOR LOOP---------------------------");
+
+		for (int i = 0; i <= b; i++) {
+			System.out.println("PRINTING FOR LOOP HERE -->  " + i);
+		}
+
+	System.out.println("-----below is ifElse---------------------------");
+
+		if (a >= b) {
 			System.out.println("You Passed The Exam");
-		}else {
+		} else {
 			System.out.println("Unfortunately,you failed to pass the exam");
+
 		}
-		}
+		
+		
 		
 		
 
 	}
-
-
+}
